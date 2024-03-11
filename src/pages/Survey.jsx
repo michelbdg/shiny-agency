bd060c960bd29d9d226bd7708e34887226f505c5
michelbdg <114735296+michelbdg@users.noreply.github.com>
2024-03-11 12:00:47 +0100

    Création du fichier freelances

--- src/pages/Survey.jsx
+++ src/pages/Survey.jsx
@@ -1,12 +1,10 @@
-import { Outlet, Link,  } from 'react-router-dom'
+import {  useParams  } from 'react-router-dom'
 function Survey() {
-  
+  const { questionNumber } = useParams()
   return (
     <div>
       <h1>Questionnaire 🧮</h1>
-      <Link to="client">Questionnaire Client</Link>
-      <Link to="freelance">Questionnaire Freelance</Link>
-      <Outlet />
+      <h2>Question {questionNumber}</h2>
     </div>
   )
 }
