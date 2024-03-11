c1c189cb2ccf1c9d9d139cc88d56ede2d9226e28
michelbdg <114735296+michelbdg@users.noreply.github.com>
2024-03-11 11:25:25 +0100

    ajout des path Survey, Client, Freelance, Home

---
+++ src/components/ClientForm/index.jsx
@@ -0,0 +1,8 @@
+function ClientForm() {
+  return (
+    <div>
+      <h2>Questionnaire Client 🧮</h2>
+    </div>
+  )
+}
+export default ClientForm
---
+++ src/components/FreelanceForm/index.jsx
@@ -0,0 +1,8 @@
+function FreelanceForm(){
+    return(
+        <div>
+            <h2>Questionnaire Freelance 🧮</h2>
+        </div>
+    )
+}
+export default FreelanceForm
