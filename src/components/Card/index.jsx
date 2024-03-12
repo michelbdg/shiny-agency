95b4b272b7b8c691f13ce2d386943a72a7f17a76
michelbdg <114735296+michelbdg@users.noreply.github.com>
2024-03-12 09:24:49 +0100

    Ajout des composant style-component dans Card

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,12 +1,24 @@
 import PropTypes from 'prop-types'
 import DefaultPicture from '../../assets/profile.png'
+import styled from 'styled-components'
 
 function Card({ label, title, picture }) {
+  const CardLabel = styled.span`
+    color: #5843e4;
+    font-size: 22px;
+    font-weight: bold;
+  `
+  const CardImage = styled.img`
+    height: 80px;
+    width: 80px;
+    border-radius: 50%;
+  `
   return (
     <div style={{ display: 'flex', flexDirection: 'column', padding: 15 }}>
-      <span>{label}</span>
-      <img src={picture} alt="freelance" height={80} width={80} />
+      <CardLabel>{label}</CardLabel>
+      <CardImage src={picture} alt="freelance" />
       <span>{title}</span>
+      
     </div>
   )
 }
@@ -16,8 +28,8 @@ Card.propTypes = {
   picture: PropTypes.string.isRequired,
 }
 Card.defautlProps = {
-    label: "",
-    title: "",
-    picture: DefaultPicture,
+  label: '',
+  title: '',
+  picture: DefaultPicture,
 }
 export default Card
