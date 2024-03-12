bfb3c8486b9e48cf898df02c7648126881d20238
michelbdg <114735296+michelbdg@users.noreply.github.com>
2024-03-12 11:21:19 +0100

    Mise en place du style sur toutes les pages

--- src/index.js
+++ src/index.js
@@ -9,20 +9,28 @@ import ClientForm from './Components/ClientForm'
 import FreelanceForm from './Components/FreelanceForm'
 import Error from './Components/Error'
 import Freelances from './pages/Freelances'
+import styled from 'styled-components'
 
+const GlobalStyle = createGlobalStyle`
+    div {
+        font-family: 'Trebuchet MS', Helvetica, sans-serif;
+    }
+`
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <Router>
-      <Header/>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/survey/:questionNumber' element={<Survey />} />
-        <Route path='*' element={<Error />} />
-        <Route path="/freelances" element={<Freelances />} />
-        <Route path='/client' element={<ClientForm />} />
-        <Route path='/freelance' element={<FreelanceForm />} />
-      </Routes>
+      <GlobalStyle>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/survey/:questionNumber" element={<Survey />} />
+          <Route path="*" element={<Error />} />
+          <Route path="/freelances" element={<Freelances />} />
+          <Route path="/client" element={<ClientForm />} />
+          <Route path="/freelance" element={<FreelanceForm />} />
+        </Routes>
+      </GlobalStyle>
     </Router>
   </React.StrictMode>
 )
