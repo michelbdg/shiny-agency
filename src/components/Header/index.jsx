364848583ff782b9b02f0951c2683516f2386e14
michelbdg <114735296+michelbdg@users.noreply.github.com>
2024-03-12 09:27:07 +0100

    Ajout du style-component dans Header avec une variable StyledLink

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,19 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
 
-function Header(){
-    return (
-        <nav>
-            <Link to="/">Accueil</Link>
-            <Link to="/survey/1">Questionnaire</Link>
-            <Link to="/freelances">Profils</Link>
-        </nav>
-    )
+const StyledLink = styled(Link)`
+  padding: 15px;
+  color: #8186a0;
+  text-decoration: none;
+  font-size: 18px;
+`
+function Header() {
+  return (
+    <nav>
+      <StyledLink to="/">Accueil</StyledLink>
+      <StyledLink to="/survey/1">Questionnaire</StyledLink>
+      <StyledLink to="/freelances">Profils</StyledLink>
+    </nav>
+  )
 }
 export default Header
